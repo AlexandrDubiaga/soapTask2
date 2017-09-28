852537928144f39192acc082eaa7a857a4b9d6a0
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-09-28 18:55:23 +0300

    Update Car.php

--- server/Car.php
+++ server/Car.php
@@ -56,7 +56,6 @@ class Car
         }
             $result = mysqli_query($this->conn, "SELECT * FROM AutoShop");
         if (sizeof($conditions)) {
-            var_dump($conditions);
         $query .=  ' WHERE ' . implode(' AND ', $conditions);
              while ($row[] = mysqli_fetch_array($query, MYSQL_ASSOC)) {}
             return $row;
