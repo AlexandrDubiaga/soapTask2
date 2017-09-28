03dcd9405ce79a8a36347db664f9a544a916a22d
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-09-28 18:22:58 +0300

    Update Car.php

--- server/Car.php
+++ server/Car.php
@@ -47,27 +47,23 @@ class Car
 
     }
     
-     public function getListOfCarsByParams($searchcontact)
+     public function getListOfCarsByParams()
     {
-         
-          $sql = mysqli_query($this->conn,"select AutoShop.id, AutoShop.marka, AutoShop.model, AutoShop.year_car, AutoShop.engine_capacity, AutoShop.color, AutoShop.max_speed, AutoShop.price  (AutoShop.id like '%$searchcontact%')
-         AS sovp1, (AutoShop.marka like '%$searchcontact%') AS sovp2, (AutoShop.model like '%$searchcontact%') AS sovp3, (AutoShop.year_car like '%$searchcontact%') AS sovp4, (AutoShop.engine_capacity like '%$searchcontact%') AS sovp5, (AutoShop.color like '%$searchcontact%') AS sovp6, (AutoShop.max_speed like '%$searchcontact%') AS sovp7, (AutoShop.price like '%$searchcontact%')  HAVING sovp1+sovp2+sovp3+sovp4+sovp5+sovp6+sovp7>=2 ");
-          while ($row[] = mysqli_fetch_array($sql, MYSQL_ASSOC)) {
-        }
-        return $row;
-         
-        /* $conditions = array();
-        $columns = array('one', 'two', 'three', 'four', 'five', 'six', 'seven');
-        for ($i = 0; $i < 8; $i++) {
+        $conditions = array();
+        $columns = array('id', 'marka', 'model', 'year_car', 'engine_capacity', 'color', 'max_speed', 'price');
+        for ($i = 0; $i < 9; $i++) {
         if (!empty(${'s' . $i})) {
          $conditions[] = sprintf("%s = '%s'", $columns[$i], ${'s' . $i});
         }
         }
-            $query = 'SELECT * FROM invoices';
+            $query = 'SELECT * FROM AutoShop';
         if (sizeof($conditions)) {
-     $query .= ' WHERE ' . implode(' AND ', $conditions);
-}
-         */
+        $query .= ' WHERE ' . implode(' AND ', $conditions);
+        }
+          while ($row[] = mysqli_fetch_array($query, MYSQL_ASSOC)) {
+        }
+        return $row;
+         
          
          
          
