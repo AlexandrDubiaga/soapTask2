189d10cd5e3d3278d5809dbbfcaeb1016ddcae96
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-09-29 17:39:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,23 @@ $arr = array('color'=>'red');
 var_dump($obj->getListOfCarsByParams($arr));
 
 $arr2 = array('id_car'=>'2','name'=>'Solo','sername'=>'Valex','pay'=>'Web');
-$obj->getOrderCar($arr2)
+$obj->getOrderCar($arr2);
 
+
+
+try {
+$server = new SoapServer('server/wsdl.wsdl');
+$server->setClass("Car");
+$server->setPersistence(SOAP_PERSISTENCE_SESSION);
+$server->handle();
+} catch (ExceptionFileNotFound $e) {
+echo 'Error message: ' . $e->getMessage();
+}
+$client = new
+SoapClient(
+"http://192.168.0.15/~user2/SOAP/soapTask2/index.php?a=Car&amp;amp;action=wsdl"
+);
+echo $client->getListOfCars();
 
 
 ?>
