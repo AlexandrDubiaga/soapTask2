bd59a185577d057b52b2bf744762f6d51bd1fbe7
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-02 17:18:17 +0300

    Update Client.php

--- client/Client.php
+++ client/Client.php
@@ -1,7 +1,7 @@
 <?php
 include('../config.php');
 $client = new SoapClient('http://192.168.0.15/~user2/SOAP/soapTask2/server/MySoap.wsdl',array('connection_timeout' => 120));
-print_r($client->getListOfCars());
+var_dump($client->getListOfCars());
 
 include('template/index.html');
 
