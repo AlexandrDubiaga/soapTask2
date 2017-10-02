ee883abcfe4ead2f2d63178b9266284b8e8df846
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-02 17:23:41 +0300

    Update Client.php

--- client/Client.php
+++ client/Client.php
@@ -1,6 +1,5 @@
 <?php
 include('../config.php');
-print_r($client->__getFunction());
 $client = new SoapClient('http://192.168.0.15/~user2/SOAP/soapTask2/server/MySoap.wsdl',array('connection_timeout' => 120));
 var_dump($client->__getFunctions());
 
